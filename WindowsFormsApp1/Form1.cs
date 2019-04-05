2ae5c767be599606a643ee752e96d2005415bc57
songminwoo <songnumber1@naver.com>
2019-04-05 13:40:02 +0900

    3

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -17,10 +17,6 @@ namespace WindowsFormsApp1
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("1");
-            MessageBox.Show("2");
-        }
+        private void button1_Click(object sender, EventArgs e) => MessageBox.Show("1");
     }
 }
